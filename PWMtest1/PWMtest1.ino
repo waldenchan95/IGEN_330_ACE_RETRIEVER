fc24cee7eda9c649328c754b8a3c8ef7d0fbd386
lucagarner <lucargarner@gmail.com>
2024-01-29 16:57:32 -0800

    PWM test updated to work with motorcotnrols
    See title

--- PWMtest1/PWMtest1.ino
+++ PWMtest1/PWMtest1.ino
@@ -2,15 +2,7 @@
 
 #include <math.h>
 #include <PWM.h>
-
-// PWM duty cycle values for input to motor controllers
-#define PWM_LOW 95
-#define PWM_HIGH 20
-
-// initialize variables
-
-int x_position_ooi; //x position of object of interest
-int y_position_ooi; //y position of object of interest
+#include "/Users/lucagarner/Coding/IGEN 330/IGEN_330_ACE_RETRIEVER/MotorControls/MotorControls.ino"
 
 // drive variables
 const int conR = 10;
@@ -20,18 +12,8 @@ int input_frequency = 150;
 
 // control variables
 
-int x_error = 0;
-int y_error = 0;
-
 int RightMotorSpeed;
 int LeftMotorSpeed;
-
-void set_pwm_frequency(int frequency) {
-
-  bool set_con_L_in = SetPinFrequencySafe(conL, frequency);
-  Serial.print("  Setting conL Frequency: ");
-  Serial.print(set_con_L_in);
-}
 
 void setup() {
   // initialize output pins
@@ -42,28 +24,8 @@ void setup() {
   
   // start serial communication
   Serial.begin(9600);
-  set_pwm_frequency(input_frequency);
+  set_pwm_frequency(conL, input_frequency);
 
-}
-
-// Motor Functions:
-// Takes -255 to 255 inputs and maps them to correct PWM for the robots motors
-void RMotor (int speed) {
-  int adjSpeed = map(speed, -255, 255, PWM_LOW, PWM_HIGH);
-  pwmWrite(conR, adjSpeed);
-  Serial.print("RightSpeed: ");
-  Serial.print(Speed);
-  Serial.print("   Right_realspeed: ");
-  Serial.println(adjSpeed);
-}
-
-void LMotor (int speed) {
-  int adjSpeed = map(speed, -255, 255, PWM_LOW, PWM_HIGH);
-  pwmWrite(conL, adjSpeed);
-  Serial.print("  LeftSpeed: ");
-  Serial.print(Speed);
-  Serial.print("   Left_realspeed: ");
-  Serial.println(adjSpeed);
 }
 
 void loop() {
@@ -75,24 +37,24 @@ void loop() {
     RightMotorSpeed = i;
     LeftMotorSpeed = i;
   
-    RMotor(RightMotorSpeed);
-    LMotor(LeftMotorSpeed);
+    RMotor(conR, RightMotorSpeed);
+    LMotor(conL, LeftMotorSpeed);
     delay(18);
   }
   for (int i = 100; i > -100; i -= 3) {
     RightMotorSpeed = i;
     LeftMotorSpeed = i;
   
-    RMotor(RightMotorSpeed);
-    LMotor(LeftMotorSpeed);
+    RMotor(conR, RightMotorSpeed);
+    LMotor(conL, LeftMotorSpeed);
     delay(18);
   }
   for (int i = -100; i <= 0; i += 3) {
     RightMotorSpeed = i;
     LeftMotorSpeed = i;
   
-    RMotor(RightMotorSpeed);
-    LMotor(LeftMotorSpeed);
+    RMotor(conR, RightMotorSpeed);
+    LMotor(conL, LeftMotorSpeed);
     delay(18);
   }
 }
