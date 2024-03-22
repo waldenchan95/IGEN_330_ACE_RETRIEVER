5ec2d0153ee9e77fc14d57472d5ed40392a5efa1
lucagarner <lucargarner@gmail.com>
2024-03-21 18:27:33 -0700

    maming odometr into header

--- Odometry.h
+++ Odometry.h
@@ -2,79 +2,214 @@
 // Include in main file
 // User will have constantly updated x, y, and a (angle) position data
 
-// Right encoder pins
-#define R_CLK 2
-#define R_DT 4
+#include <Wire.h>
+#include <Adafruit_LIS3MDL.h>
+#include <Adafruit_Sensor.h>
+//Requires math.h
 
-// Left encoder pins
-#define L_CLK 3
-#define L_DT 5
+#define CLKS_PER_SAMPLE 4 // pure counts of encoder
+#define DIST_PER_CLK 0.005984734 // 3inch wheel, 40 clicks per rotation (m)
 
-// Constants
-#define SAMPLE_RATE 80 // (ms)
+//ENCODER
+// Rotary Encoder Module connections
+// const int rDT = 7;    // DATA signal
+// const int rCLK = 19;    // CLOCK signal
+// const int lDT = 6;    // DATA signal
+// const int lCLK = 18;    // CLOCK signal
+// Store previous Pins state
+extern int rPreviousCLK;   
+extern int rPreviousDATA;
+extern int lPreviousCLK;   
+extern int lPreviousDATA;
+// Store current counter value
+extern int rcounter = 0;
+extern int lcounter = 0;
+// Position variables
+extern double x = 0; //(m)
+extern double y = 0; //(m)
+extern double a = 0; //(rad)
 
-extern const double botWidth = 0.4; // m
-extern const double dPerClk = 0.01;
+//MAGNETOMETER
+// Hard-iron calibration settings
+extern const float hard_iron[3] = {
+  -12.04,  -15.64,  13.31
+};
 
+// Soft-iron calibration settings
+extern const float soft_iron[3][3] = {
+  {  1.0,  0.0, 0.0  },
+  {  0.0,  1.0, 0.0  },
+  {  0.0,  0.0, 1.0  }
+};
 
-extern double x = 0;
-extern double y = 0;
-extern double a = 1.570796327;
-extern unsigned long lastEncoderSample = 0;
-extern int rCounter = 0;
-extern int lCounter = 0;
+extern static float heading = 0;
 
-// ISR's
-void rMove() {  
-    if (digitalRead(R_DT) == 1) {
-      rCounter++;
-    }
-    if (digitalRead(R_DT) == 0) {
-      rCounter--;  
-    } 
-}
+// //Create instance of magnetometer
+// Adafruit_LIS3MDL lis3mdl;
 
-void lMove() {  
-    if (digitalRead(L_DT) == 1) {
-      lCounter++;
-    }
-    if (digitalRead(L_DT) == 0) {
-      lCounter--;  
-    } 
-}
-
-// Interrupt initialization
 void StartOdometry() {
-  attachInterrupt(digitalPinToInterrupt(R_CLK),  rMove, FALLING);
-  attachInterrupt(digitalPinToInterrupt(L_CLK),  lMove, FALLING);
-  pinMode(R_DT,INPUT);
-  pinMode(L_DT,INPUT);
+  attachInterrupt(digitalPinToInterrupt(rCLK), rEncMove, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(lCLK), lEncMove, CHANGE);
+
+  rPreviousCLK = digitalRead(rCLK);
+  rPreviousDATA = digitalRead(rDT);
+  lPreviousCLK = digitalRead(lCLK);
+  lPreviousDATA = digitalRead(lDT);
+
+  if (!lis3mdl.begin_I2C()) {
+    Serial.println("ERROR: Could not find magnetometer");
+  }
 }
 
-// Odometry
+void rEncMove() {
+  check_rotary_R();
+  rPreviousCLK = digitalRead(rCLK);
+  rPreviousDATA = digitalRead(rDT);
+}
+
+void lEncMove() {
+  check_rotary_L();
+  lPreviousCLK = digitalRead(lCLK);
+  lPreviousDATA = digitalRead(lDT);
+}
+
 void Odometry() {
 
-  double rDistance = rCounter * dPerClk;
-  double lDistance = lCounter * dPerClk;
+//COMPASS
+  static float hi_cal[3];
+  // Get new sensor event with readings in uTesla
+  sensors_event_t event;
+  lis3mdl.getEvent(&event);
+  // Put raw magnetometer readings into an array
+  float mag_data[] = {event.magnetic.x,
+                      event.magnetic.y,
+                      event.magnetic.z};
+  // Apply hard-iron offsets
+  for (uint8_t i = 0; i < 3; i++) {
+    hi_cal[i] = mag_data[i] - hard_iron[i];
+  }
+  // Apply soft-iron scaling
+  for (uint8_t i = 0; i < 3; i++) {
+    mag_data[i] = (soft_iron[i][0] * hi_cal[0]) +
+                  (soft_iron[i][1] * hi_cal[1]) +
+                  (soft_iron[i][2] * hi_cal[2]);
+  }
+  // Calculate angle for heading, assuming board is parallel to
+  // the ground and  Y points toward heading.
+  heading = -1.0 * (atan2(mag_data[0], mag_data[1])*180)/PI;
+  a = heading/180*PI;
+  // Convert heading to 0..360 degrees
+  if (a < 0) {
+    a += 2*PI;
+  }
   
-  if (millis() - lastEncoderSample > SAMPLE_RATE) {
-    lastEncoderSample = millis();
-    double da = atan( (rDistance - lDistance) / botWidth);
-    double dd = (rDistance + lDistance) / 2.0;
-    a = a + da;
+  if (abs(rcounter) > CLKS_PER_SAMPLE || abs(lcounter) > CLKS_PER_SAMPLE) {
+    double rdistance = rcounter * DIST_PER_CLK;
+    double ldistance = lcounter * DIST_PER_CLK;
+    double dd = (rdistance + ldistance) / 2.0;
     double dx = cos(a) * dd;
     double dy = sin(a) * dd;
     x = x + dx;
     y = y + dy;
     
-    rCounter = 0;
-    lCounter = 0;
-
-    Serial.print("  x = ");
-    Serial.print(x, 4);
-    Serial.print("  y = ");
-    Serial.print(y, 4);
-    Serial.print("  angle = ");
-    Serial.println(a, 4);
+    rcounter = 0;
+    lcounter = 0;
   }
+
 }
+
+//FUNCTIONS
+// Check if Rotary Encoder was moved on thr right side
+void check_rotary_R() {
+
+ if ((rPreviousCLK == 0) && (rPreviousDATA == 1)) {
+    if ((digitalRead(rCLK) == 1) && (digitalRead(rDT) == 0)) {
+      rcounter++;
+      return;
+    }
+    if ((digitalRead(rCLK) == 1) && (digitalRead(rDT) == 1)) {
+      rcounter--;
+      return;
+    }
+  }
+
+if ((rPreviousCLK == 1) && (rPreviousDATA == 0)) {
+    if ((digitalRead(rCLK) == 0) && (digitalRead(rDT) == 1)) {
+      rcounter++;
+      return;
+    }
+    if ((digitalRead(rCLK) == 0) && (digitalRead(rDT) == 0)) {
+      rcounter--;
+      return;
+    }
+  }
+
+if ((rPreviousCLK == 1) && (rPreviousDATA == 1)) {
+    if ((digitalRead(rCLK) == 0) && (digitalRead(rDT) == 1)) {
+      rcounter++;
+      return;
+    }
+    if ((digitalRead(rCLK) == 0) && (digitalRead(rDT) == 0)) {
+      rcounter--;
+      return;
+    }
+  }  
+
+if ((rPreviousCLK == 0) && (rPreviousDATA == 0)) {
+    if ((digitalRead(rCLK) == 1) && (digitalRead(rDT) == 0)) {
+      rcounter++;
+      return;
+    }
+    if ((digitalRead(rCLK) == 1) && (digitalRead(rDT) == 1)) {
+      rcounter--;
+      return;
+    }
+  }       
+}
+
+void check_rotary_L() {
+
+ if ((lPreviousCLK == 0) && (lPreviousDATA == 1)) {
+    if ((digitalRead(lCLK) == 1) && (digitalRead(lDT) == 0)) {
+      lcounter--;
+      return;
+    }
+    if ((digitalRead(lCLK) == 1) && (digitalRead(lDT) == 1)) {
+      lcounter++;
+      return;
+    }
+  }
+
+if ((lPreviousCLK == 1) && (lPreviousDATA == 0)) {
+    if ((digitalRead(lCLK) == 0) && (digitalRead(lDT) == 1)) {
+      lcounter--;
+      return;
+    }
+    if ((digitalRead(lCLK) == 0) && (digitalRead(lDT) == 0)) {
+      lcounter++;
+      return;
+    }
+  }
+
+if ((lPreviousCLK == 1) && (lPreviousDATA == 1)) {
+    if ((digitalRead(lCLK) == 0) && (digitalRead(lDT) == 1)) {
+      lcounter--;
+      return;
+    }
+    if ((digitalRead(lCLK) == 0) && (digitalRead(lDT) == 0)) {
+      lcounter++;
+      return;
+    }
+  }  
+
+if ((lPreviousCLK == 0) && (lPreviousDATA == 0)) {
+    if ((digitalRead(lCLK) == 1) && (digitalRead(lDT) == 0)) {
+      lcounter--;
+      return;
+    }
+    if ((digitalRead(lCLK) == 1) && (digitalRead(lDT) == 1)) {
+      lcounter++;
+      return;
+    }
+  }       
+ }
