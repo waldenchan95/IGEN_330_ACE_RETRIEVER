d7fa30921f79561499d3422d6e7a579491e1e867
lucagarner <lucargarner@gmail.com>
2024-03-30 10:27:09 -0700

    LOW PASS FILTER

--- ACE_Project/ACE_Project.ino
+++ ACE_Project/ACE_Project.ino
@@ -24,7 +24,7 @@ unsigned long init_ball_seen_time; // When robot sees ball in pixy it waits a sh
 
 // Odometry Definitions
 #define CLKS_PER_SAMPLE 4 // pure counts of encoder
-#define DIST_PER_CLK 0.005984734*240/207 // 3inch wheel, 40 clicks per rotation (m)
+#define DIST_PER_CLK 0.005984734*240/207 // calibrated real world (m)
 
 // PINS
 // Motor
@@ -118,8 +118,95 @@ const double dK = 60; // MASTER GAIN drive
 const double dKi = 34; // integral multiplier
 const double dKd = 2; // derivative multiplier
 
+/// LOW_PASS FILTER CLASS
+template <int order> // order is 1 or 2
+class LowPass
+{
+  private:
+    float a[order];
+    float b[order+1];
+    float omega0;
+    float dt;
+    bool adapt;
+    float tn1 = 0;
+    float x[order+1]; // Raw values
+    float y[order+1]; // Filtered values
+
+  public:  
+    LowPass(float f0, float fs, bool adaptive){
+      // f0: cutoff frequency (Hz)
+      // fs: sample frequency (Hz)
+      // adaptive: boolean flag, if set to 1, the code will automatically set
+      // the sample frequency based on the time history.
+      
+      omega0 = 6.28318530718*f0;
+      dt = 1.0/fs;
+      adapt = adaptive;
+      tn1 = -dt;
+      for(int k = 0; k < order+1; k++){
+        x[k] = 0;
+        y[k] = 0;        
+      }
+      setCoef();
+    }
+
+    void setCoef(){
+      if(adapt){
+        float t = micros()/1.0e6;
+        dt = t - tn1;
+        tn1 = t;
+      }
+      
+      float alpha = omega0*dt;
+      if(order==1){
+        a[0] = -(alpha - 2.0)/(alpha+2.0);
+        b[0] = alpha/(alpha+2.0);
+        b[1] = alpha/(alpha+2.0);        
+      }
+      if(order==2){
+        float alphaSq = alpha*alpha;
+        float beta[] = {1, sqrt(2), 1};
+        float D = alphaSq*beta[0] + 2*alpha*beta[1] + 4*beta[2];
+        b[0] = alphaSq/D;
+        b[1] = 2*b[0];
+        b[2] = b[0];
+        a[0] = -(2*alphaSq*beta[0] - 8*beta[2])/D;
+        a[1] = -(beta[0]*alphaSq - 2*beta[1]*alpha + 4*beta[2])/D;      
+      }
+    }
+
+    float filt(float xn){
+      // Provide me with the current raw value: x
+      // I will give you the current filtered value: y
+      if(adapt){
+        setCoef(); // Update coefficients if necessary      
+      }
+      y[0] = 0;
+      x[0] = xn;
+      // Compute the filtered values
+      for(int k = 0; k < order; k++){
+        y[0] += a[k]*y[k+1] + b[k]*x[k];
+      }
+      y[0] += b[order]*x[order];
+
+      // Save the historical values
+      for(int k = order; k > 0; k--){
+        y[k] = y[k-1];
+        x[k] = x[k-1];
+      }
+  
+      // Return the filtered value    
+      return y[0];
+    }
+};
+
+/// END OF LOW_PASS FILTER CLASS
+
 //Create instance of magnetometer
 Adafruit_LIS3MDL lis3mdl;
+
+// Filter instance
+LowPass<2> lp(3,1e3,true);
 
 void setup() {
   // initialize output pins
@@ -541,26 +628,25 @@ void Odometry() {
   }
   
   a = heading*PI/180;
-//  // Convert heading to 0-2pi degrees
-//  if (a < 0) {
-//    a += 2*PI;
-//  }
 
   //a = a - startingAngle;
 
-  // Filter Signal
-  if (millis() > flt_last_time + 1) {
-    flt_last_time = millis();
-    
-    a_filtered = a*flt_coeff[0];
-    for (int i = 0; i < 14; i++) {
-      a_filtered += flt_prev[i]*flt_coeff[i+1];
-    }
-    for (int i = 13; i > 0; i--) {
-      flt_prev[i] = flt_prev[i-1];
-    }
-    flt_prev[0] = a;
-  }
+  // Compute the filtered signal
+  a_filtered = lp.filt(a);  
+  
+//  // WSF Filter Signal
+//  if (millis() > flt_last_time + 1) {
+//    flt_last_time = millis();
+//    
+//    a_filtered = a*flt_coeff[0];
+//    for (int i = 0; i < 14; i++) {
+//      a_filtered += flt_prev[i]*flt_coeff[i+1];
+//    }
+//    for (int i = 13; i > 0; i--) {
+//      flt_prev[i] = flt_prev[i-1];
+//    }
+//    flt_prev[0] = a;
+//  }
 
   if (abs(rcounter) > CLKS_PER_SAMPLE || abs(lcounter) > CLKS_PER_SAMPLE) {
     double rdistance = rcounter * DIST_PER_CLK;
